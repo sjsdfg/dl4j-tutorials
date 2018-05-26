41a8dba6a65252706e1dc1853eb8b9f470358930
sjsdfg <736777445@qq.com>
2018-05-26 14:13:38 +0800

    更新日志

--- src/main/java/lesson6/LenetMnistExample.java
+++ src/main/java/lesson6/LenetMnistExample.java
@@ -2,27 +2,22 @@ package lesson6;
 
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.Updater;
-import org.deeplearning4j.nn.conf.WorkspaceMode;
 import org.deeplearning4j.nn.conf.inputs.InputType;
-import org.deeplearning4j.nn.conf.layers.ConvolutionLayer;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.layers.SubsamplingLayer;
+import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by agibsonccc on 9/16/15.
@@ -48,17 +43,18 @@ public class LenetMnistExample {
             Construct the neural network
          */
         log.info("Build model....");
+
+        // learning rate schedule in the form of <Iteration #, Learning Rate>
+        Map<Integer, Double> lrSchedule = new HashMap<>();
+        lrSchedule.put(0, 0.01);
+        lrSchedule.put(1000, 0.005);
+        lrSchedule.put(3000, 0.001);
+
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(seed)
                 .l2(0.0005)
-                /*
-                    Uncomment the following for learning decay and bias
-                 */
-                //.learningRateDecayPolicy(LearningRatePolicy.Inverse).lrPolicyDecayRate(0.001).lrPolicyPower(0.75)
                 .weightInit(WeightInit.XAVIER)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .updater(new Nesterovs(0.01, 0.9))
-                .trainingWorkspaceMode(WorkspaceMode.SINGLE)
                 .list()
                 .layer(0, new ConvolutionLayer.Builder(5, 5)
                         //nIn and nOut specify depth. nIn here is the nChannels and nOut is the number of filters to be applied
@@ -67,7 +63,7 @@ public class LenetMnistExample {
                         .nOut(20)
                         .activation(Activation.IDENTITY)
                         .build())
-                .layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .layer(1, new SubsamplingLayer.Builder(PoolingType.MAX)
                         .kernelSize(2,2)
                         .stride(2,2)
                         .build())
@@ -77,7 +73,7 @@ public class LenetMnistExample {
                         .nOut(50)
                         .activation(Activation.IDENTITY)
                         .build())
-                .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                .layer(3, new SubsamplingLayer.Builder(PoolingType.MAX)
                         .kernelSize(2,2)
                         .stride(2,2)
                         .build())
@@ -108,29 +104,17 @@ public class LenetMnistExample {
         model.init();
 
 
-        long startTime = System.currentTimeMillis();
-
         log.info("Train model....");
-        model.setListeners(new ScoreIterationListener(100));
+        model.setListeners(new ScoreIterationListener(10)); //Print score every 10 iterations
         for( int i=0; i<nEpochs; i++ ) {
             model.fit(mnistTrain);
             log.info("*** Completed epoch {} ***", i);
 
             log.info("Evaluate model....");
-            Evaluation eval = new Evaluation(outputNum);
-            while(mnistTest.hasNext()){
-                DataSet ds = mnistTest.next();
-                INDArray output = model.output(ds.getFeatureMatrix(), false);
-                eval.eval(ds.getLabels(), output);
-
-            }
+            Evaluation eval = model.evaluate(mnistTest);
             log.info(eval.stats());
             mnistTest.reset();
         }
-
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("total time:" + (endTime - startTime));
         log.info("****************Example finished********************");
     }
 }
