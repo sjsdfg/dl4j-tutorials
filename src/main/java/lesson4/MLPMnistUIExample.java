4e4f39e99cbd7cc9471370bdeedd465db62662f8
sjsdfg <736777445@qq.com>
2018-06-09 14:34:23 +0800

    update

---
+++ src/main/java/lesson4/MLPMnistUIExample.java
@@ -0,0 +1,108 @@
+package lesson4;
+
+import org.deeplearning4j.api.storage.StatsStorage;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.ui.api.UIServer;
+import org.deeplearning4j.ui.stats.StatsListener;
+import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by Joe on 2018/6/9.
+ */
+public class MLPMnistUIExample {
+
+    private static Logger log = LoggerFactory.getLogger(MLPMnistSingleLayerExample.class);
+
+    public static void main(String[] args) throws IOException {
+        //number of rows and columns in the input pictures
+        final int numRows = 28;
+        final int numColumns = 28;
+        int outputNum = 10; // number of output classes
+        int batchSize = 128; // batch size for each epoch
+        int rngSeed = 123; // random number seed for reproducibility
+        int numEpochs = 15; // number of epochs to perform
+        int listenerFrequency = 1;
+
+
+        //Get the DataSetIterators:
+        DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
+        DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
+
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(rngSeed) //include a random seed for reproducibility
+                // use stochastic gradient descent as an optimization algorithm
+                .updater(new Nesterovs(0.006, 0.9))
+                .l2(1e-4)
+                .list()
+                .layer(0, new DenseLayer.Builder() //create the first, input layer with xavier initialization
+                        // batchSize, features
+                        .nIn(numRows * numColumns)
+                        .nOut(1000)
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD) //create hidden layer
+                        .nIn(1000)
+                        .nOut(outputNum)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .pretrain(false).backprop(true) //use backpropagation to adjust weights
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        //Initialize the user interface backend
+        // 获取一个UI实例
+        UIServer uiServer = UIServer.getInstance();
+
+        //Configure where the network information (gradients, activations, score vs. time etc) is to be stored
+        //Then add the StatsListener to collect this information from the network, as it trains
+        // 训练的存储位置
+        StatsStorage statsStorage = new InMemoryStatsStorage();             //Alternative: new FileStatsStorage(File) - see UIStorageExample
+
+        //Attach the StatsStorage instance to the UI: this allows the contents of the StatsStorage to be visualized
+        uiServer.attach(statsStorage);
+        model.init();
+        //print the score with every 1 iteration
+        model.setListeners(new StatsListener(statsStorage, listenerFrequency)
+                ,new ScoreIterationListener(1)
+        );
+
+        log.info("Train model....");
+        for( int i=0; i<numEpochs; i++ ){
+            model.fit(mnistTrain);
+        }
+
+
+        log.info("Evaluate model....");
+        Evaluation eval = new Evaluation(outputNum); //create an evaluation object with 10 possible classes
+        while(mnistTest.hasNext()){
+            DataSet next = mnistTest.next();
+            INDArray output = model.output(next.getFeatureMatrix(), false); //get the networks prediction
+            eval.eval(next.getLabels(), output); //check the prediction against the true class
+        }
+
+        log.info(eval.stats());
+        log.info("****************Example finished********************");
+    }
+}
