9a665088421a8654abb96c7050e84bfe75d7ae6d
sjsdfg <736777445@qq.com>
2018-06-14 21:16:20 +0800

    将单通道的 INDArray 保存为灰度图

--- src/main/java/lesson6/UsingModelToPredict.java
+++ src/main/java/lesson6/UsingModelToPredict.java
@@ -4,6 +4,8 @@ import org.datavec.image.loader.NativeImageLoader;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.io.ClassPathResource;
 
@@ -31,6 +33,9 @@ public class UsingModelToPredict {
         File imgFile = new ClassPathResource("/mnist/4.jpg").getFile();
 
         INDArray imgNdarray = imageLoader.asMatrix(imgFile);
+
+//        DataNormalization scaler = new ImagePreProcessingScaler(0, 1);
+//        scaler.transform(imgNdarray);
         System.out.println(imgNdarray);
 
         System.out.println(Arrays.toString(imgNdarray.shape()));
@@ -41,17 +46,22 @@ public class UsingModelToPredict {
         MultiLayerNetwork network = ModelSerializer.restoreMultiLayerNetwork(modelPath, false);
 
         INDArray output = network.output(imgNdarray);
+//
+//        System.out.println(output);
+//        System.out.println(Nd4j.getBlasWrapper().iamax(output));
+//
+//        int[] results = network.predict(imgNdarray);
+//        System.out.println(Arrays.toString(results));
 
-        System.out.println(output);
-        System.out.println(Nd4j.getBlasWrapper().iamax(output));
-
-        int[] results = network.predict(imgNdarray);
-        System.out.println(Arrays.toString(results));
-
-        System.out.println(imageLoader.asMatrix(new File("model/test.jpg")));
+//        System.out.println(imageLoader.asMatrix(new File("model/test.jpg")));
     }
 
 
+    /**
+     * 将单通道的 INDArray 保存为灰度图
+     * @param array 输入
+     * @return 灰度图转化
+     */
     private static BufferedImage imageFromINDArray(INDArray array) {
         int[] shape = array.shape();
 
@@ -62,12 +72,11 @@ public class UsingModelToPredict {
             for (int y = 0; y < height; y++) {
                 int gray = array.getInt(0, 0, y, x);
 
-                //handle out of bounds pixel values
-//                gray = Math.min(gray, 1);
-////
-//                gray = Math.max(gray, 0);
+                // handle out of bounds pixel values
+                gray = Math.min(gray, 255);
+                gray = Math.max(gray, 0);
 
-                image.setRGB(x, y, gray);
+                image.getRaster().setSample(x, y, 0, gray);
             }
         }
         return image;
